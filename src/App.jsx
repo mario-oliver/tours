197db1f72961e8c1f0a4cc7bb72ee787a01ec7b3
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-05-23 11:12:19 -0400

    setting up our fetch call with useEffect for proper rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState(true);
+
+  // the second argument will prevent us from an infinate loop of refetching data. if we enter that loop the API will prevent us from reaccessing for 15 minutes
+  useEffect(() => {}, []);
+
   return <h2>Tours Starter</h2>;
 };
 export default App;

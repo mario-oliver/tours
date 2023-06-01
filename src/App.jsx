b50a5d3538a03cf1df963af9898bb7fa554e4cd2
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-06-01 12:36:16 -0400

    conditional rendering for loading and fetching datat

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Tours from './Tours';
 
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [tours, setTours] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const result = await response.json();
+        setTours(result);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
 
   // the second argument will prevent us from an infinate loop of refetching data. if we enter that loop the API will prevent us from reaccessing for 15 minutes
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
-  return <h2>Tours Starter</h2>;
+  return (
+    <div>
+      <h2>Tours Starter</h2>
+      {isLoading ? <h2>Loading...</h2> : <Tours tours={tours}></Tours>}
+    </div>
+  );
 };
 export default App;
--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Tours = () => {
+const Tours = (tours) => {
+  let { id, image, info, name, price } = tours;
   return <h2>Tours</h2>;
 };
 
